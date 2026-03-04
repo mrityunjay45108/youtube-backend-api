2ce6666813e52f6e6ce89ab7b41b774430735a7c
mrityunjay45108 <kumarmrityunjay5210@gmail.com>
2026-03-04 21:04:50 +0530

    first commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({ message: "User route working ✅" });
+});
+
+export default router;
